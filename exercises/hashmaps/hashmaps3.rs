30a8dc752884466b0dfb107e7eb6c1a28f8c8c82
zephoon <zephoonyu@protonmail.com>
2022-11-06 09:06:33 +0800

    feat: add hashmap and rc1

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -40,6 +39,18 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        scores.entry(team_1_name.clone()) // Update Team 1
+            .and_modify(|t| { // t: Team
+                t.goals_scored += team_1_score;
+                t.goals_conceded += team_2_score;
+            })
+            .or_insert(Team {name: team_1_name, goals_scored: team_1_score, goals_conceded: team_2_score});
+        scores.entry(team_2_name.clone()) // Update Team 2
+            .and_modify(|t| {
+                t.goals_scored += team_2_score;
+                t.goals_conceded += team_1_score;
+            })
+            .or_insert(Team {name: team_2_name, goals_scored: team_2_score, goals_conceded: team_1_score});
     }
     scores
 }
